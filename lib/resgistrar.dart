89dbc49e34930f50255285f746e3d10405592c85
colettidaniela <coletti.daniela.epet20@gmail.com>
2021-08-29 21:19:03 -0300

    update2

--- lib/resgistrar.dart
+++ lib/resgistrar.dart
@@ -4,6 +4,21 @@ class Registrar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        brightness: Brightness.light,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back,
+            size: 20,
+            color: Colors.black,
+          ),
+        ),
+      ),
       body: Container(),
     );
   }
