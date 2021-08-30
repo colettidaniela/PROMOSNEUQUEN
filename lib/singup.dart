44902def1e5f7b01278cf0515fd838e4cfcdc3ec
colettidaniela <coletti.daniela.epet20@gmail.com>
2021-08-29 23:08:57 -0300

    update0

---
+++ lib/singup.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+class singup extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        brightness: Brightness.light,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back,
+            size: 25,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        child: Column(
+          children: <Widget>[
+            Column(
+              children: [
+                SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  "INICIAR SESION",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Container(
+              padding: EdgeInsets.all(50),
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(labelText: "Dueño del local"),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: "DNI"),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(40),
+                  border: Border(
+                    bottom: BorderSide(color: Colors.black),
+                    top: BorderSide(color: Colors.black),
+                    left: BorderSide(color: Colors.black),
+                    right: BorderSide(color: Colors.black),
+                  )),
+              child: MaterialButton(
+                color: Colors.deepPurpleAccent,
+                elevation: 0,
+                minWidth: 450,
+                height: 55,
+                shape: RoundedRectangleBorder(
+                    side: BorderSide(color: Colors.deepPurpleAccent),
+                    borderRadius: BorderRadius.circular(40)),
+                child: Text("INICIAR SESION",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w900,
+                      fontSize: 20,
+                    )),
+                onPressed: () {},
+              ),
+            ),
+            SizedBox(
+              height: 6,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("Todavia no tenes una cuenta?"),
+                Text(
+                  "Registrate",
+                  style: TextStyle(
+                    color: Colors.blue[400],
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
