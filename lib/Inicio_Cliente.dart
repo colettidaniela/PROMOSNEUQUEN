05ab7ffe8fdadfe26c5f16bdf0cafe4fc3ed471f
colettidaniela <coletti.daniela.epet20@gmail.com>
2021-08-30 12:28:44 -0300

    update1

--- lib/Inicio_Cliente.dart
+++ lib/Inicio_Cliente.dart
@@ -5,6 +5,34 @@ class InicioCliente extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurpleAccent,
+        elevation: 0,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              title: Text(
+                'Categorias',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text(
+                'Crear cuenta(Dueño de local)',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
       bottomNavigationBar: CurvedNavigationBar(
         color: Colors.deepPurpleAccent,
         backgroundColor: Colors.white,
